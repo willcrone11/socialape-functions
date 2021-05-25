e0e2aba1e8a769fc58475f5df77e71c2e54fdb7c
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-05-25 15:27:14 -0400

    fixes variable name to match argument

--- functions/utility/validators.js
+++ functions/utility/validators.js
@@ -23,7 +23,7 @@ exports.validateSignupData = (data) => {
 
   if(isEmpty(data.password)) errors.password = 'Must not be empty'
   if(data.password !== data.confirmPassword) errors.confirmPassword = 'Passwords must match'
-  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+  if(isEmpty(data.handle)) errors.handle = 'Must not be empty'
 
   return {
     errors,
