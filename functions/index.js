fe4041549be118db169a4499ecdf7b1aec1c9840
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-05-26 15:45:06 -0400

    Adds functionality for liking, unliking, and deleting screams

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,31 @@ const app = require('express')();
 
 const FBAuth = require('./utility/fbAuth');
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
+const { 
+  getAllScreams, 
+  postOneScream, 
+  getScream, 
+  commentOnScream, 
+  likeScream, 
+  unlikeScream,
+  deleteScream
+} = require('./handlers/screams');
+
+const { 
+  signup, 
+  login, 
+  uploadImage, 
+  addUserDetails, 
+  getAuthenticatedUser 
+} = require('./handlers/users');
 
 //Scream Routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
-//delete scream
-//like a scream
-//unlike a scream
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
 
 //Users Routes
