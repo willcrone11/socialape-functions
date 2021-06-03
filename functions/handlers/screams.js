3123768d980e2e6a0ff29734b9d512a6862d1752
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-06-03 17:51:40 -0400

    Cleans up code

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -13,6 +13,9 @@ exports.getAllScreams = (req, res) => {
           body: doc.data().body,
           userHandle: doc.data().userHandle,
           createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount,
+          userImage: doc.data().userImage,
         });
       });
       return res.json(screams);
@@ -80,7 +83,8 @@ exports.getScream = (req, res) => {
 
 //comment on a scream
 exports.commentOnScream = (req, res) => {
-  if(req.body.body.trim() === '') return res.status(400).json({ error: 'Must not be empty'});
+  if(req.body.body.trim() === '') 
+    return res.status(400).json({ comment: 'Must not be empty'});
   
   const newComment = {
     body: req.body.body,
